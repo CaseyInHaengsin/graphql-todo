996a4cbc7c27b926f4f691446ccfe839880bd340
Hunter Osmun <Hunter@Osmun.net>
2022-02-18 09:35:47 -0700

    gql setup

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,28 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Query {
+    list: [Item!]
+  }
+
+  type Item {
+    text: String!
+    id: ID!
+    category: String
+  }
+`
+
+const resolvers = {}
+
+const database = [
+  { text: 'list item 1', id: 1 },
+  { text: 'list item two', id: 2 },
+  { text: 'list item III', id: 3 }
+]
+
 const PORT = 7733
 
-const express = require('express'),
-  app = express(),
-  cors = require('cors')
-
-app.use(cors())
-app.use(express.json())
-
-app.listen(PORT, () => console.log(`Running on ${PORT}`))
+const server = new ApolloServer({ typeDefs, resolvers })
+server.listen({ port: PORT }).then(({ url }) => {
+  console.log(`Server is ready at ${url}`)
+})
