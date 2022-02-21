e730867f4cf06e614edcee78d499c56ee926add5
caseyinhaengsin <rowley.james74@gmail.com>
2022-02-21 15:20:42 -0700

    added json-server for storage and updated queries and mutation to hit api.

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,18 @@
-const PORT = 7733
+const PORT = 4000
 const http = require('http')
+const axios = require('axios')
 const express = require('express'),
   app = express(),
   cors = require('cors')
 
 const { v4: uuidv4 } = require('uuid')
+
 const { ApolloServer, gql } = require('apollo-server-express')
 const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core')
 
-let tasks = [
-  {
-    id: '1',
-    task: 'Do cool things'
-  },
-  {
-    id: '2',
-    task: 'Do another cool thing'
-  }
-]
+const api = axios.create({
+  baseURL: 'http://localhost:3000'
+})
 
 const typeDefs = gql`
   type Task {
@@ -41,40 +36,59 @@ const typeDefs = gql`
 `
 const resolvers = {
   Query: {
-    getTasks: (parent, args, ctx, info) => {
+    getTasks: async (parent, args, ctx, info) => {
       const { id, search } = args
       if (id && search)
         throw new Error(
           'pick a lane and stay in it. Use an ID or a search term'
         )
       if (id) {
-        return tasks.find(task => task.id === id)
+        const taskResp = await ctx.api.get(`/tasks/${id}`)
+        if (taskResp?.status !== 200) {
+          throw new Error('Failed to get user')
+        }
+
+        return [taskResp?.data]
       }
       if (search) {
-        return tasks.filter(task => task.task.toLowerCase().includes(search))
+        const tasksResp = await ctx.api.get('/tasks')
+        if (tasksResp?.status !== 200) {
+          throw new Error('failed to get users')
+        }
+
+        return tasksResp?.data?.filter(task =>
+          task.task.toLowerCase().includes(search)
+        )
       }
-      return tasks
+      const tasksResp = await ctx.api.get('/tasks')
+      if (tasksResp?.status !== 200) {
+        throw new Error('failed to get users')
+      }
+      return tasksResp?.data
     }
   },
 
   Mutation: {
-    addTodo: (parent, args, ctx, info) => {
+    addTodo: async (parent, args, ctx, info) => {
       const id = uuidv4()
-      const task = { task: args?.task, id: id }
-      tasks = [...tasks, task]
-      return task
-    },
-    updateTodo: (parent, args, ctx, info) => {
-      tasks = tasks.filter(task => task.id !== args?.id)
-      const pretendUpdatedTask = { id: args?.id, task: args?.task }
-      tasks = [...tasks, pretendUpdatedTask]
-      return pretendUpdatedTask
-    },
-    deleteTodo: (parent, args, ctx, info) => {
-      tasks = tasks.filter(task => task.id !== args?.id)
 
+      const taskResp = await ctx.api.post('/tasks', { task: args?.task })
+      return taskResp?.data
+    },
+    updateTodo: async (parent, args, ctx, info) => {
+      // tasks = tasks.filter(task => task.id !== args?.id)
+      // const pretendUpdatedTask = { id: args?.id, task: args?.task }
+      const updatedTask = await ctx.api.put(`/tasks/${args?.id}`, {
+        task: args?.task
+      })
+
+      return updatedTask?.data
+    },
+    deleteTodo: async (parent, args, ctx, info) => {
+      const deletedTaskResp = await ctx.api.delete(`/tasks/${args?.id}`)
+      console.log('deleteresponse', deletedTaskResp)
       return {
-        message: `yay, you deleted the task with id ${args?.id}`
+        message: deletedTaskResp?.statusText
       }
     }
   }
@@ -86,6 +100,9 @@ async function startApolloServer (typeDefs, resolvers) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: ({ req }) => ({
+      api
+    }),
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
   })
   await server.start()
